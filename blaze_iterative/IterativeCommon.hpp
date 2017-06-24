018f75bb988cd3d6b56b230912e9fbbd0ad7e005
Tyler Olsen <tylerjohnolsen@gmail.com>
2017-06-24 09:11:39 -0400

    First commit

---
+++ blaze_iterative/IterativeCommon.hpp
@@ -0,0 +1,16 @@
+//
+// Created by tyler on 6/24/17.
+//
+
+#ifndef BLAZE_ITERATIVE_ITERATIVE_COMMON_HPP
+#define BLAZE_ITERATIVE_ITERATIVE_COMMON_HPP
+
+#include "Blaze.h"
+
+#define BLAZE_NAMESPACE_OPEN namespace blaze {
+#define BLAZE_NAMESPACE_CLOSE }
+
+#define ITERATIVE_NAMESPACE_OPEN namespace iterative {
+#define ITERATIVE_NAMESPACE_CLOSE }
+
+#endif //BLAZE_ITERATIVE_ITERATIVE_COMMON_HPP
