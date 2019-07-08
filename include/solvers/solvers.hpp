9613d7464e80708be350b57c24a8e5f15090d5ff
NanmiaoWu <nanmiaowu@gmail.com>
2019-07-08 15:54:23 -0500

    Arnoldi (#2)
    * change if() requirement, set it equal to zero
    
    * Tiny eigenvalues are different
    
    * add test for Arnoldi
    
    * Test for 10 by 10 Matrix, only tiny values(e-15) are different
    
    * Remove the comment of #include<utility> & align the spacing to match with other attributes

--- include/solvers/solvers.hpp
+++ include/solvers/solvers.hpp
@@ -1,5 +1,6 @@
 // Copyright (c)   2017 Tyler Olsen
 //                 2018 Patrick Diehl
+//                 2019 Nanmiao Wu
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
@@ -12,5 +13,7 @@
 #include "BiCGSTAB.hpp"
 #include "PreconditionBiCGSTABTag.hpp"
 #include "PreconditionBiCGSTAB.hpp"
+#include "Arnoldi.hpp"
+#include "ArnoldiTag.hpp"
 
 #endif //BLAZE_ITERATIVE_SOLVERS_HPP
--- tests/main.cpp
+++ tests/main.cpp
@@ -9,29 +9,74 @@ using namespace blaze;
 using namespace blaze::iterative;
 // Copyright (c)   2017 Tyler Olsen
 //                 2018 Patrick Diehl
+//                 2019 Nanmiao Wu
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 int main() {
+    /*
+    * // Test for ConjugateGradient
+    * // Test for BiCGSTAB
+    * // Test for PreconditionBiCGSTAB
+   std::size_t N = 1000;
+   DynamicMatrix<double,columnMajor> A(N,N, 0.0);
+   DynamicVector<double> b(N, 0.0);
 
-    std::size_t N = 1000;
+   for(int i=0; i<N; ++i) {
+       A(i,i) = 2.0;
+       b[i] = 1.0*(1+i);
+   }
+
+    *
+   //ConjugateGradientTag tag;
+   //BiCGSTABTag tag;
+
+
+   //std::cout << solve(A,b,tag) << std::endl << std::endl;
+
+   PreconditionBiCGSTABTag tag;
+   tag.do_log() = true;
+   std::cout << solve(A,b,tag, "Cholesky") << std::endl << std::endl;
+
+   int iter(0);
+   for(auto r : tag.convergence_history()) {
+       std::cout << iter++ << '\t' << r << '\n';
+   }
+   */
+
+    // Test Arnoldi
+    std::size_t N = 10;
+
     DynamicMatrix<double,columnMajor> A(N,N, 0.0);
-    DynamicVector<double> b(N, 0.0);
-
-    for(int i=0; i<N; ++i) {
+    for(int i=1; i<N-1; ++i) {
+        A(i, i-1) = -1.0;
         A(i,i) = 2.0;
-        b[i] = 1.0*(1+i);
+        A(i,i+1) = -1.0;
     }
+    A(0,1) = -1.0;
+    A(0,0) = 2.0;
+    A(9,8) = -1.0;
+    A(9,9) = 2.0;
+    std::cout<< "The Matrix A is: " << std::endl << A << std::endl;
 
-    PreconditionBiCGSTABTag tag;
-    tag.do_log() = true;
+    DynamicVector<double> b(N, 1.0);
+    DynamicVector<complex<double>,columnVector> w(N); // The vector for the real eigenvalues
+    DynamicMatrix<complex<double>,rowMajor> V(N,N); // The matrix for the left eigenvectors
+    eigen(A,w,V);
+    std::cout<< "The eigenvalues of Matrix A is: " << std::endl << w << std::endl;
+    //std::cout << "The eigenvectors of Matrix A is: " << V << std::endl;
 
-    std::cout << solve(A,b,tag) << std::endl << std::endl;
+    DynamicVector<complex<double>,columnVector> w1(N); // The vector for the real eigenvalues
+    DynamicMatrix<complex<double>,rowMajor> V1(N,N); // The matrix for the left eigenvectors
 
-    int iter(0);
-    for(auto r : tag.convergence_history()) {
-        std::cout << iter++ << '\t' << r << '\n';
-    }
+    std::size_t n = N-1;
+    //DynamicMatrix<double> Q(N, (n + 1));
+    //DynamicMatrix<double> h((n + 1), n);
+    ArnoldiTag tag;
+    auto res = solve(A,b,tag,n);
+    //eigen(h,w1,V1);
+    std::cout << "The Matrix h is: " <<std::endl << res.second << std::endl;
+
 
     return 0;
 }
