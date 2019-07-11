5a12087a9765bab72d4d82d6ebf4fe03a34d25d4
nanmiao <nanmiaowu@gmail.com>
2019-07-11 16:00:16 -0500

    incomplete CF: loop j from 0 to i

--- include/solvers/PreconditionCG.hpp
+++ include/solvers/PreconditionCG.hpp
@@ -96,7 +96,7 @@ ITERATIVE_NAMESPACE_OPEN
 
                 std::size_t n = A.columns();
                 for(std::size_t i = 0; i < n; ++i){
-                    for(std::size_t j = 0; j < n; ++j){
+                    for(std::size_t j = 0; j <= i; ++j){
                         if (A(i, j) == 0)
                             K(i, j) = 0;
                     }
