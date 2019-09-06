73e3dd8efcb1f9c36e3dc71fdb15283cc85c1f66
nanmiao <nanmiaowu@gmail.com>
2019-09-06 11:31:03 -0500

    Add test for Arnoldi

--- include/BlazeIterative.hpp
+++ include/BlazeIterative.hpp
@@ -1,5 +1,6 @@
 // Copyright (c)   2017 Tyler Olsen
 //                 2018 Patrick Diehl
+//                 2019 Nanmiao Wu
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
@@ -11,5 +12,7 @@
 #include "solve.hpp"
 
 #include "solvers/solvers.hpp"
+
+#define epsilon  1.0e-7
 
 #endif //BLAZE_ITERATIVE_BLAZEITERATIVE_HPP
--- include/solve.hpp
+++ include/solve.hpp
@@ -1,5 +1,6 @@
 // Copyright (c)   2017 Tyler Olsen
 //                 2018 Patrick Diehl
+//                 2019 Nanmiao Wu
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
